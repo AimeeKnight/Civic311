c09b63efde270bfc40d9815ca5f08dbb8eeeac0f
Aimee Knight <aimeeknight83@gmail.com>
2014-03-24 22:00:48 -0500

    try travis-encrypt again

--- test/unit/resident_spec.js
+++ test/unit/resident_spec.js
@@ -53,7 +53,7 @@ describe('resident', function(){
       var u1 = new Resident({name:'Aimee', email:'aimeemarieknight@gmail.com', password:'9876'});
       //var u1 = new Resident({name:'Aimee', email:'aimeemarieknight@nomail.com', password:'9876'});
       u1.register(function(err, body){
-        expect(err).to.be.undefined;
+        expect(err).to.be.null;
         expect(u1.password).to.have.length(60);
         expect(u1._id).to.be.instanceof(Mongo.ObjectID);
         //body = JSON.parse(body);
