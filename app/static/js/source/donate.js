5787d65de7134994ebfd6c524eeed06f812728bf
Aimee Knight <aimeeknight83@gmail.com>
2014-03-27 15:46:14 -0500

    fix gravatar looping on donors page

--- app/static/js/source/donate.js
+++ app/static/js/source/donate.js
@@ -9,6 +9,7 @@
     $('#id-status').click(checkStatusById);
     $('#complete').geocomplete();
     $('#donation').change(updateAmount);
+    getEmails();
     //$.fn.geocomplete('#complete');
     /*
     Dropzone.options.myAwesomeDropzone = {
@@ -21,8 +22,30 @@
     };
     */
 
-    var email = $('#email').val();
-    $('.gravatar-img').append($.gravatar(email));
+  }
+  
+  function getEmails(){
+    var emails = $('.email');
+    var names = $('.donor-name');
+    emails.hide();
+    names.hide();
+    var emailStrings = [];
+    var nameStrings = [];
+
+    $.each(emails, function(index, email){
+      emailStrings.push(email.innerHTML);
+    });
+
+    $.each(names, function(index, name){
+      nameStrings.push(name.innerHTML);
+    });
+
+    $.each(emailStrings, function(index, string){
+      $('.gravatar-img').append($.gravatar(string));
+      var $name = $('<span class="gravatar"></span>');
+      $name.text(nameStrings[index]);
+      $('.gravatar-img').append($name);
+    });
   }
 
   function checkStatusById(){
